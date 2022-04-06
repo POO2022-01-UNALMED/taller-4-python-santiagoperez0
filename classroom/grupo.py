dfe587f1961fb4226359295cdd419c1647a508b3
santiagoperez0 <jperezpi@unal.edu.co>
2022-04-06 00:13:18 -0500

    ssssss

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -6,23 +6,25 @@ from classroom.asignatura import Asignatura
 
 class Grupo:
 
-    grado = None
+    grado = "Grado 12"
 
-    def __init__(self, grupo="grupo ordinado", asignaturas=None, estudiantes=None):
+    def __init__(self, grupo="grupo ordinado", asignaturas=[], estudiantes=None):
         self._grupo = grupo
         self._asignaturas = asignaturas
         self.listadoAlumnos = estudiantes
 
-    def listadoAsignaturas(self, kwargs):
+    def listadoAsignaturas(self, **kwargs):
         for x in kwargs.values():
             self._asignaturas.append(Asignatura(x))
 
     def agregarAlumno(self, alumno, lista=[]):
-        if(lista is None):
-            lista.append(alumno)
-            self.listadoAlumnos = self.listadoAlumnos + lista
+        if lista is None:
+            if self.listadoAlumnos is None:
+                self.listadoAlumnos = [alumno]
+            else:
+                self.listadoAlumnos += [alumno]
         else:
-            self.listadoAlumnos = [alumno]
+            self.listadoAlumnos = lista + [alumno]
 
     @ classmethod
     def asignarNombre(cls, nombre="Grado 10"):
@@ -32,6 +34,11 @@ class Grupo:
     def asignarNombre(cls, nombre="Grado 6"):
         cls.grado = nombre
 
-    @ classmethod
-    def asignarNombre(cls, nombre="Grado 4"):
-        cls.grado = nombre
+
+    def __str__(self):
+        texto = "Grupo de estudiantes: "
+        if self._grupo == "grupo ordinado":
+            texto += "grupo predeterminado"
+        else:
+            texto += self._grupo
+        return
